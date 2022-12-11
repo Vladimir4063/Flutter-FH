7931ad71182e8a6becea14a7b10bd104cbca4bbb
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2022-12-11 00:49:36 -0300

    Review Extends

---
+++ holamundo/bin/10-extends.dart
@@ -0,0 +1,32 @@
+import '9-clases-abstractas.dart';
+
+void main() {
+  final superman = new Heroe('Clark Kent');
+  final villano = new Villano('Lex Luthor');
+
+  print(superman);
+  print(villano);
+
+}
+
+abstract class Personaje{
+  String? poder;
+  String nombre;
+
+  Personaje(this.nombre);
+
+  @override 
+  String toString(){
+    return '$nombre - $poder';
+  }
+}
+
+class Heroe extends Personaje{
+  int valentia =100;
+  Heroe(String nombre): super(nombre);
+}
+
+class Villano extends Personaje{
+  int maldad = 50;
+  Villano(String nombre):super(nombre);
+}
